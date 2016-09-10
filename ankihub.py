fd0726efe4058a81582c52f72c33d1e14fa68f80
dayjaby <dayjaby@gmail.com>
2016-09-10 08:31:59 +0200

    added progressbar

--- ankihub.py
+++ ankihub.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore,QtGui
 import httplib
-import requests
+import urllib2
+#import requests
 import json
 import os
 import sys
@@ -13,23 +14,29 @@ headers = {"User-Agent": "AnkiHub"}
 dataPath = os.path.expanduser('~/.ankihub.json')
 
 class DialogUpdates(QtGui.QDialog, Ui_DialogUpdates):
-    def __init__(self, parent, data, oldData, callback):
+    def __init__(self, parent, data, oldData, callback, ask=True):
         QtGui.QDialog.__init__(self,parent)
         self.setupUi(self)
         totalSize = sum(map(lambda x:x['size'],data['assets']))
 
-        def answer(doUpdate,answ):
-            callback(doUpdate,answ)
+        def answer(doUpdate,answ,append):
+            callback(doUpdate,answ,append)
             self.close()
 
-        self.connect(self.update,QtCore.SIGNAL('clicked()'),
-                     lambda:answer(True,'ask'))
-        self.connect(self.dont,QtCore.SIGNAL('clicked()'),
-                     lambda:answer(False,'ask'))
-        self.connect(self.always,QtCore.SIGNAL('clicked()'),
-                     lambda:answer(True,'always'))
-        self.connect(self.never,QtCore.SIGNAL('clicked()'),
-                     lambda:answer(False,'never'))
+        self.html = u''
+        self.appendHtml(markdown(data['body']))
+
+        if ask:
+            self.connect(self.update,QtCore.SIGNAL('clicked()'),
+                         lambda:answer(True,'ask',self.appendHtml))
+            self.connect(self.dont,QtCore.SIGNAL('clicked()'),
+                     lambda:answer(False,'ask',self.appendHtml))
+            self.connect(self.always,QtCore.SIGNAL('clicked()'),
+                     lambda:answer(True,'always',self.appendHtml))
+            self.connect(self.never,QtCore.SIGNAL('clicked()'),
+                     lambda:answer(False,'never',self.appendHtml))
+        else:
+            answer(True,'ask',self.appendHtml)
 
         self.labelUpdates.setText(
             unicode(self.labelUpdates.text()).format(
@@ -37,12 +44,10 @@ class DialogUpdates(QtGui.QDialog, Ui_DialogUpdates):
                 oldData.get('tag_name') or unicode(),
                 data['tag_name']))
 
-        html = '<html><body>'
-        html += markdown(data['body'])
-        html += '</body></html>'
-        self.textBrowser.setHtml(html)
 
-
+    def appendHtml(self,html):
+        self.html += html
+        self.textBrowser.setHtml(u'<html><body>{0}</body></html>'.format(self.html))
 
 
 def asset(a):
@@ -58,15 +63,31 @@ def _profileLoaded():
 addHook("profileLoaded",_profileLoaded)
 
 def updateSingle(repositories,path,data):
-    def callback(doUpdate,answer):
+    def callback(doUpdate,answer,appendHtml):
         if doUpdate:
             repositories[path] = data
             for asset in data['assets']:
                 code = asset['url']
                 p, fname = os.path.split(code)
-                d = requests.get(code)
+                appendHtml('<br />Start downloading {0}<br />'.format(fname))
+                response = urllib2.urlopen(code)
+                meta = response.info()
+                file_size = int(meta.getheaders("Content-Length")[0])
+                d = buffer('')
+                dl = 0
+                i = 10
+                while True:
+                    dkb = response.read(1024)
+                    if not dkb:
+                        break
+                    i += 1
+                    dl += len(dkb)
+                    d += dkb
+                    if i % 10 == 0:
+                        appendHtml('{0} % downloaded<br/>'.format(int(dl*100/file_size)))
+                    QtGui.QApplication.instance().processEvents()
                 def installData():
-                    aqt.mw.addonManager.install(d.content,fname)
+                    aqt.mw.addonManager.install(d,fname)
                     repositories[path]['update'] = answer
                     with open(dataPath,'w') as file:
                         json.dump(repositories,file,indent=2)
